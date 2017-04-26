08a9b2e9f3c7104fa7e0ca3858548800e2d80128
caylazabel <caylazabel@gmail.com>
2017-04-26 13:46:23 -0700

    profile router begann

--- route/profile-router.js
+++ route/profile-router.js
@@ -12,3 +12,31 @@ const createError = require('http-errors');
 
 const Profile = require('../model/profile.js');
 const bearerAuth = require('../lib/bearer-auth-middleware');
+
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const s3 = new AWS.S3();
+const dataDir = `${__dirname}/../data`;
+const upload = multer({ dest: dataDir });
+
+const profileRouter = module.exports = Router();
+
+function s3UploadProm(params) {
+  debug('s3UploadProm');
+  return new Promise((resolve, reject) => {
+    s3.upload(params, (err, s3data) => {
+      if (err) return reject(err);
+      resolve(s3data);
+    });
+  });
+}
+
+profileRouter.post('/api/profile', bearerAuth, upload.single('image'), jsonParser, function(req, res, next){
+  debug('POST: /api/profile');
+  if(!req.file) {
+    return next(createError(400, 'file not saved'));
+  }
+
+  
+})
