1d25f6bc475c9fbaedeb5e4f5b0c9909f5bd4257
caylazabel <caylazabel@gmail.com>
2017-05-02 15:54:54 -0700

    trying to get tests to pass

---
+++ test/lib/test-env.js
@@ -0,0 +1,9 @@
+'use strict';
+
+process.env.PORT='3000';
+process.env.API_URL='http://localhost:3000';
+process.env.MONGODB_URI='mongodb://localhost/villans_vs_superheroes';
+process.env.APP_SECRET='random';
+process.env.AWS_BUCKET='villans-superheroes';
+process.env.AWS_ACCESS_KEY_ID='TESTACCESSKEY';
+process.env.AWS_SECRET_ACCESS_KEY='SECRETKEYTEST';
